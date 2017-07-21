13e17509e5dade80377f2606c82b7b0e92967e0f
shades3002 <shades3002@gmail.com>
2017-07-21 00:15:00 -0400

    Fetching Data with a Url

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,14 @@ class App extends Component {
         const BASE_URL = "https://api.spotify.com/v1/search?";
         const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
         console.log('FETCH_URL', FETCH_URL);
+        fetch(FETCH_URL, {
+            method: 'GET',
+            headers: new Headers({
+            "Authorization": "Bearer BQDdWRzzFxigFv5Xjtdp3OKO_bTIiFwI0-jWCjzV4_nx6WFX7931xI7s2keD6uJmqDAEMbZw6HgbfJG9MSXaKHtT7sGr_Vfuj5RYKmSN38GTtdxAHKMgWpwCHdEyrq8_mwDgV9KWH9ekpWSwycTtw26tEP8D5rZZ-aRSEODv-1R4XlpKf0I&refresh_token=AQDJMsy923GLRW-wVsMBKFbFgoo-TTQTJJWKXh3mceSVxjL57WmeJlKFYHEEY5bCXwkZHtFUKcFV9horPVzCP0Mjqy37PFiak2m0YL9NIKmOCUKZHUqWJ5uWn6YFiSU8KBY"
+            })
+        })
+        .then(response => response.json())
+        .then(json => console.log('json', json));
     }
 
     render() {
