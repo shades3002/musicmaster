1f002a6f9fd81906cca866b698061318c8a0ec32
shades3002 <shades3002@gmail.com>
2017-07-21 00:36:01 -0400

    Building the Artist Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
+import Profile from './Profile';
 
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            artist: null
         }
     }
 
@@ -23,7 +25,11 @@ class App extends Component {
             })
         })
         .then(response => response.json())
-        .then(json => console.log('json', json));
+        .then(json => {
+            const artist = json.artists.items[0];
+            console.log('artist', artist);
+            this.setState({artist});
+        });
     }
 
     render() {
@@ -48,10 +54,9 @@ class App extends Component {
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
-                <div className="Profile">
-                    <div>Artist Picture</div>
-                    <div>Artist Name</div>
-                </div>
+                <Profile
+                  artist={this.state.artist}
+                />
                 <div className="Gallery">
                     Gallery
                 </div>
